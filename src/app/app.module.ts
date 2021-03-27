80cfb978775730bc49797bfc9313f518f2b779f5
Priya Bharmbe <priyabharambe70@gmail.com>
2021-03-27 20:14:47 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,40 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { TopbarComponent } from './topbar/topbar.component';
+import { HeaderComponent } from './header/header.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
+import { ServicesComponent } from './services/services.component';
+import { ClientComponent } from './client/client.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { TestimonialComponent } from './testimonial/testimonial.component';
+import { ContactComponent } from './contact/contact.component';
+import { OurteamComponent } from './ourteam/ourteam.component';
+import { CalltoactionComponent } from './calltoaction/calltoaction.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopbarComponent,
+    HeaderComponent,
+    Section1Component,
+    Section2Component,
+    ServicesComponent,
+    ClientComponent,
+    PortfolioComponent,
+    TestimonialComponent,
+    ContactComponent,
+    OurteamComponent,
+    CalltoactionComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
